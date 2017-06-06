53e8fb53b893a014294599d4a864bd14be219261
Slava Peshko <slava.peshko@gmail.com>
2017-06-06 22:47:16 +0300

    +added mapping

---
+++ Shop.BLL/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Shop.Common.Models;
+using Shop.Common.ViewModels;
+
+namespace Shop.BLL.AutoMapper
+{
+	public class DomainToViewModelMappingProfile : Profile
+	{
+		public DomainToViewModelMappingProfile()
+		{
+			CreateMap<Category, CategoryViewModel>();
+			CreateMap<Product, ProductViewModel>();
+		}
+	}
+}
---
+++ Shop.BLL/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Shop.Common.Models;
+using Shop.Common.ViewModels;
+
+namespace Shop.BLL.AutoMapper
+{
+	public class ViewModelToDomainMappingProfile : Profile
+	{
+		public ViewModelToDomainMappingProfile()
+		{
+			//CreateMap<ProductFromViewModel, Product>()
+			//	.ConstructUsing(n=> new )
+		}
+	}
+}
